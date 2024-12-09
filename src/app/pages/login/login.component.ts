1a70cbfdffd7a3907f7eec80e9c42d11630110a4
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2024-12-09 16:19:24 +0100

    Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Add required modules
+  imports: [CommonModule, FormsModule,ToastrModule], // Add required modules
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
-
+constructor(private readonly toastr: ToastrService){}
   onLogin() {
     console.log('Login successful:', this.email, this.password);
+    this.toastr.success('Hello world!', 'Toastr fun!');
   }
 }

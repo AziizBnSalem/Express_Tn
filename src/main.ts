16b1763ce048e73fcd2bc0ffb1d91373a1851320
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2024-12-09 16:18:50 +0100

    Main Pages

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes),provideAnimations(), // required animations providers
+    provideToastr(),],
 }).catch((err) => console.error(err));

6247257bab37c9e47489626d4972c91b2c7b56eb
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2024-12-16 14:07:44 +0100

    App Routes Updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';  // Updated import
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent }  // Updated to HomeComponent

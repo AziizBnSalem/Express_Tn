2fed5f8275269e1bbe00e833db38c082674e9eea
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2024-12-16 14:07:54 +0100

    Login Pages

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,20 +2,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule,ToastrModule], // Add required modules
+  imports: [CommonModule, FormsModule, ToastrModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
-constructor(private readonly toastr: ToastrService){}
+
+  constructor(private readonly toastr: ToastrService, private readonly router: Router) {} // Inject Router
+
   onLogin() {
     console.log('Login successful:', this.email, this.password);
-    this.toastr.success('Hello world!', 'Toastr fun!');
+    this.toastr.success('Login successful!', 'Welcome!');
+    this.router.navigate(['/home']); // Redirect to Home page
   }
 }
